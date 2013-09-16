26288156bfdb48825de907e9920d8347a508f148
Polytonic <kfung@tinycranes.com>
2013-09-15 21:36:20 -0400

    Initial Commit

---
+++ Chlorine/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Chlorine
+//
+//  Created by Polytonic on 9/15/13.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
