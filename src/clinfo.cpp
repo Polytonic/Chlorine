83c0a0ef5b60c9abf37174b4fa7a89ca4338c865
Polytonic <kfung@tinycranes.com>
2013-09-28 17:17:28 -0400

    Add Skeleton `clinfo.cpp`

---
+++ src/clinfo.cpp
@@ -0,0 +1,19 @@
+// Standard Library Headers
+#include <iostream>
+
+// Platform Specific Headers
+#ifdef __APPLE__
+#include <OpenCL/OpenCL.h>
+#else
+#include <CL/OpenCL.h>
+#endif
+
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, CL!\n";
+    return 0;
+}
+
