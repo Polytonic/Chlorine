9e68a97dff06f9230aafe8eadd6939e01aa06657
Polytonic <kfung@tinycranes.com>
2013-10-05 19:22:28 -0400

    Add Preliminary Exception Handling

--- src/chlorine.cpp
+++ src/chlorine.cpp
@@ -7,9 +7,15 @@
 
 int main(int argc, const char * argv[])
 {
-    std::cout << argc << std::endl;
-    std::cout << sizeof(*argv) << std::endl;
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    try {
+
+        std::cout << argc << std::endl;
+        std::cout << sizeof(*argv) << std::endl;
+
+    } catch (cl::Error exception) {
+        chlorine::print_exception_string(exception);
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
