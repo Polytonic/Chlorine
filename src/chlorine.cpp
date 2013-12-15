0f7245fdc9354207cbcbbfa89d6a4a30b32b063c
Polytonic <kfung@tinycranes.com>
2013-12-15 17:53:35 -0500

    Delete Unused OpenCL C++ Bindings

--- src/chlorine.cpp
+++ src/chlorine.cpp
@@ -21,9 +21,29 @@ int main(int argc, char * argv[])
         ch::Queue = cl::CommandQueue(ch::Context, ch::Device,
                     ch::profiling ? CL_QUEUE_PROFILING_ENABLE : 0);
 
+        // 32-bit Floating Point Precision
+        if (ch::precision == 4) {
+        switch (ch::vec_width)  {
+            case 1:  ch::execute<cl_float, cl_float>();   break;
+            case 2:  ch::execute<cl_float, cl_float2>();  break;
+            case 3:  ch::execute<cl_float, cl_float3>();  break;
+            case 4:  ch::execute<cl_float, cl_float4>();  break;
+            case 8:  ch::execute<cl_float, cl_float8>();  break;
+            case 16: ch::execute<cl_float, cl_float16>(); break;
+            default: std::cerr << "Invalid Vector Width"; break;
+        }}
 
-        std::cout << "Hello World";
-
+        // 64-bit Floating Point Precision
+        else if (ch::precision == 8) {
+        switch  (ch::vec_width)      {
+            case 1:  ch::execute<cl_double, cl_double>();   break;
+            case 2:  ch::execute<cl_double, cl_double2>();  break;
+            case 3:  ch::execute<cl_double, cl_double3>();  break;
+            case 4:  ch::execute<cl_double, cl_double4>();  break;
+            case 8:  ch::execute<cl_double, cl_double8>();  break;
+            case 16: ch::execute<cl_double, cl_double16>(); break;
+            default: std::cerr << "Invalid Vector Width";   break;
+        }}
 
     } catch (cl::Error exception) {
         ch::print_exception_string(exception);
