09d47586e70537e1b1569ea9cc5e788ca750cf7f
Polytonic <kfung@tinycranes.com>
2014-04-29 20:34:50 -0400

    Update Kernel Addition Test

--- chlorine/chlorine.cpp
+++ chlorine/chlorine.cpp
@@ -3,23 +3,26 @@
 
 int main()
 {
-    std::vector<int>    spam  (10, 3);
-    std::vector<int>    ham   (10, 5);
+    // Test Vector of Ints
+    std::vector<int>    spam  (10, 1);
+    std::vector<int>    ham   (10, 2);
+
     std::vector<float>  eggs  (10, 2.7182f);
-    std::valarray<float>  bacon (99, 10); // valarray constructor is backwards
-    std::array<int, 10> spinach = { 9, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
-    int cheese[10] = { 1, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
+    std::valarray<float>  bacon (3.1415f, 10); // valarray constructor is backwards
+
+    std::array<int, 10> spinach = { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
+    int cheese[10] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
 
 
     try{
         ch::Worker busybot(0,0);
         busybot.set_kernel("tests/default.cl");
-        busybot.execute("add", spam, bacon, 12.1f, cheese);
+        busybot.execute("add", spam, ham, 1, eggs, bacon, 1.0f, spinach, cheese);
     } catch (cl::Error err) { std::cout << err.err() << ":" << err.what() << std::endl; }
 
-    std::cout << spam[0] << std::endl;
-    std::cout << eggs[0] << std::endl;
-    std::cout << bacon[0] << std::endl;
+    std::cout << spam[0] << std::endl;  // Should Print 4
+    std::cout << eggs[0] << std::endl;  // Should Print 6.8597
+    std::cout << spinach[0] << std::endl; // Should Print 7
     return 0;
 }
     // implement << operator for piping in kernel output?
--- tests/default.cl
+++ tests/default.cl
@@ -1,7 +1,11 @@
 
-__kernel void add(__global int * a, __global float * b, float c, __global int * d)
+__kernel void add(__global int * a, __global int * b, int c,
+  __global float * d, __global float * e, float f,
+  __global int * g, __global int * h)
+
 {
     unsigned int i = get_global_id(0);
-    a[i] += d[i];
-    b[i] += c;
+    a[i] += b[i] + c;
+    d[i] += e[i] + f;
+    g[i] += h[i] + c;
 }
