2a0b7857204feac112baee723bdf0a5580b72efe
Kevin Fung <kfung@tinycranes.com>
2015-01-13 14:01:42 -0500

    Test the Operations Selector

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -2,6 +2,13 @@
 #define CATCH_CONFIG_MAIN
 #include "test_suite.hpp"
 
+// Verifies the Default Case for Code Coverage Purposes
+TEST_CASE("Test Operations Selector, [dummy, ops]")
+{
+    CHECK(test_ops<int>("add", 1, 2) ==  3);
+    CHECK(test_ops<int>("___", 0, 0) == -1);
+}
+
 /*
     We define repeated test cases since Catch doesn't support templated tests.
     Refer to https://github.com/philsquared/Catch/issues/46 for details.
@@ -16,20 +23,3 @@ TEST_CASE("Multiplies Integers", "[int, mul]") { test<int>("integers", "mul"); }
 TEST_CASE("Adds       Floats", "[float, add]") { test<float>("floats", "add"); }
 TEST_CASE("Subtracts  Floats", "[float, sub]") { test<float>("floats", "sub"); }
 TEST_CASE("Multiplies Floats", "[float, mul]") { test<float>("floats", "mul"); }
-
-
-
-/* for every container, test every type mapping
-    should check if doubles supported
-    bool
-    unsigned char
-    char
-    unsigned short
-    short
-    unsigned int
-    int
-    unsigned long
-    long
-    half
-    float
- */
