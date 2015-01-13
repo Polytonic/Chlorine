7ae9b0c21c7a1b689432528633a05c9c299d93e5
Kevin Fung <kfung@tinycranes.com>
2015-01-13 15:05:11 -0500

    Add a Scalar Primitives Test

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -16,3 +16,34 @@ TEST_CASE("Multiplies Integers", "[int, mul]") { test<int>("integers", "mul"); }
 TEST_CASE("Adds       Floats", "[float, add]") { test<float>("floats", "add"); }
 TEST_CASE("Subtracts  Floats", "[float, sub]") { test<float>("floats", "sub"); }
 TEST_CASE("Multiplies Floats", "[float, mul]") { test<float>("floats", "mul"); }
+
+// Chlorine Scalar Primitives Test
+TEST_CASE("Primitives", "[primitives]")
+{
+    // Create a Worker and Load the Correct Types Kernel
+    ch::Worker worker; worker >> ch::read("tests/test_primitives.cl");
+    srand(static_cast<unsigned>(time(0)));
+    size_t const n = 5;
+
+    SECTION("Scalar Integers")
+    {
+        std::cerr << "Testing Scalar Function: ifill\n";
+        int b = rand();
+        std::vector<int> a(n);
+        worker.call("ifill", a, b);
+        for (unsigned int i = 0; i < a.size(); i++)
+            CHECK(a[i] == b);
+        std::cerr << std::endl;
+    }
+
+    SECTION("Scalar Floats")
+    {
+        std::cerr << "Testing Scalar Function: ffill\n";
+        float b = rand() / static_cast<float>(RAND_MAX) * 100.0f;
+        std::vector<float> a(n);
+        worker.call("ffill", a, b);
+        for (unsigned int i = 0; i < a.size(); i++)
+            CHECK(a[i] == b);
+        std::cerr << std::endl;
+    }
+}
