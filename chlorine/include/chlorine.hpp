61d1fc90080630453d4b9dfe8300af7a1c9e76ad
Kevin Fung <kfung@tinycranes.com>
2015-09-04 00:14:36 -0700

    Bump License Year

--- chlorine/include/chlorine.hpp
+++ chlorine/include/chlorine.hpp
@@ -1,7 +1,7 @@
 /**
     The MIT License (MIT)
 
-    Copyright (c) 2014 Kevin Fung
+    Copyright (c) 2015 Kevin Fung
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to
